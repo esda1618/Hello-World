d5861af2bde0cf2100e12bba1cef4c2b8f1bb06d
esda1618 <65789447+esda1618@users.noreply.github.com>
2020-08-13 17:33:24 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+	
+	public static void main(String args[]) {
+		System.out.println("Hello World");
+		System.out.println("I love listening to music");
+		System.out.println("I am a karate black belt");
+		System.out.println("I love watching TV");
+		System.out.println("My dream college is UCLA");
+		System.out.println("I plan on majoring in computer science");
+	}
+}
